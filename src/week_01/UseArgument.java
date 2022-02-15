9e0e0d39e6bcad8030357914748c8fe1c37e42f4
Bris <dangngocquan104@gmail.com>
2022-02-15 17:30:00 +0700

    Do exercise chap_01

---
+++ src/week_01/UseArgument.java
@@ -0,0 +1,9 @@
+package week_01;
+
+public class UseArgument {
+	public static void main (String[] args) {
+		System.out.print("Xin chào");
+		System.out.print(args[0]);
+		System.out.println(". Bạn khỏe không?");
+	}
+}
