0612226932b8b5e8f96e52e2f449348416e74e46
Bris <dangngocquan104@gmail.com>
2022-02-23 11:12:24 +0700

    add exe 3.2

--- src/week_02/exe_03_02.java
+++ src/week_02/exe_03_02.java
@@ -3,10 +3,21 @@ package week_02;
 import java.util.Scanner;
 
 public class exe_03_02 {
+	static int counter(String s) {
+		int ans = 0;
+		char[] c = s.toCharArray();
+		for (char i : c) ans++;
+		return ans;
+	}
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the input characte string:	");
-		String s = scanner.next();
-		
+		String s = scanner.nextLine();
+		while (s.isEmpty()) {
+			System.out.print("Enter the input characte string:	");
+			s = scanner.nextLine();
+		}
+		System.out.println(s + " has " + counter(s) + " characters." );
 	}
 }
